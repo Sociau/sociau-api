772e6544d9875d17e6a41300ac75267c7de4a8a6
Thiago Araujo <161383143+Thiago-Arauj@users.noreply.github.com>
2024-09-12 20:02:51 -0300

    Update model.py

--- src/Adoption History/model.py
+++ src/Adoption History/model.py
@@ -4,5 +4,5 @@ from sqlalchemy import text
 class Adoption_History(db.Model):
     __tablename__ = 'Adoption_History'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    person_id = db.Column('person_id', db.Integer, db.foreign_key('person_id'))
-    pet_id = db.Column('pet_id', db.Integer, db.foreign_key('pet_id'))
+    person_id = db.Column('person_id', db.Integer, db.foreign_key('person.id'))
+    pet_id = db.Column('pet_id', db.Integer, db.foreign_key('pet.id'))
