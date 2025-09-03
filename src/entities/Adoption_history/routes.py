49dff8a976d088e17531d5e01102fe6f3ef79da1
José Lucas <jlgf.profissional@gmail.com>
2025-09-03 00:05:39 -0300

    refactor: change base language

--- src/entities/Adoption_history/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from src.entities.Adoption_history.controller import AdoptionHistoryController
-
-adoption_routes = Blueprint('adoption', __name__)
-
-
-@adoption_routes.route('/adoption', methods=['GET'])
-def get_user_route():
-    return AdoptionHistoryController.get_adoption_history()
