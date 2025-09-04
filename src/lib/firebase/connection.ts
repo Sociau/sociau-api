2cefefea55373047593d4386e13ba063cd1d864d
José Lucas <jlgf.profissional@gmail.com>
2025-09-04 16:03:46 -0300

    fix: gitignore

--- src/lib/firebase/connection.ts
+++ src/lib/firebase/connection.ts
@@ -1,9 +1,5 @@
 import * as admin from "firebase-admin";
-<<<<<<< HEAD
 import * as serviceAccount from "../../../firebase-key.json";
-=======
-import * as serviceAccount from "./firebase-key.json";
->>>>>>> 54b671d (feat: firebase)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
