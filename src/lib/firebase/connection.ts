ddb2ce9bbbbebf261d166dfac1d2a88c6c467e3f
José Lucas <jlgf.profissional@gmail.com>
2025-09-04 15:58:47 -0300

    feat: firebase

--- src/lib/firebase/connection.ts
+++ src/lib/firebase/connection.ts
@@ -1,5 +1,9 @@
 import * as admin from "firebase-admin";
+<<<<<<< HEAD
 import * as serviceAccount from "../../../firebase-key.json";
+=======
+import * as serviceAccount from "./firebase-key.json";
+>>>>>>> 54b671d (feat: firebase)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
